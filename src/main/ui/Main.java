00cefbad879e123f22ed9d962d16416c8aed76be
Naman <nmiglani@students.cs.ubc.ca>
2024-04-25 14:11:33 -0700

    - Fixed grammar errors in README.md

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -4,13 +4,6 @@ import java.io.FileNotFoundException;
 import javax.swing.*;
 
 public class Main {
-//    public static void main(String[] args) {
-//        try {
-//            new FlashcardApp();
-//        } catch (FileNotFoundException e) {
-//            System.out.println("file not found");
-//        }
-//    }
     public static void main(String[] args) {
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             @Override
